ac877d37af51fcb33f6983d2d6670d8e366d2af4
luke <nerorevenge@gmail.com>
2017-05-24 01:12:45 -0400

    Added nice getFileContents Function

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
+import Control.Exception.Base
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Either
 import Lib
+import System.Directory
+
+data MyException =
+  FileNotFound
+  deriving (Show)
+
+instance Exception MyException where
+  displayException _ = "File wasn't found!"
+
+configFile :: String
+configFile = "config.yaml"
+
+-- my wrapper for getFileContents
+getFile
+  :: (Exception k)
+  => String -> EitherT k IO String
+getFile fileName =
+  liftIO (doesFileExist fileName) >>=
+  (\x ->
+     case x of
+       True -> right fileName
+       False -> throwM FileNotFound) >>=
+  (\x -> Control.Monad.Catch.catch (liftIO $ readFile x) (\y -> left $ y))
 
 main :: IO ()
-main = someFunc
+main = putStrLn "hello world"
