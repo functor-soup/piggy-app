ec38553f0c25041f72b248b3292b02538c2d0f50
luke <nerorevenge@gmail.com>
2017-05-25 22:02:20 -0400

    Working version of getting to read the config gile properly

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,14 @@ module Main where
 
 import Control.Exception.Base (displayException)
 import Control.Monad.Trans.Either
-import Data.Bifunctor
 import Lib
 import Logger
 import Utilz
+import Data.Function
 
 configFile :: String
 configFile = "config.yaml"
 
 main :: IO ()
 main =
-  print (runEitherT $ getFile configFile >>= bimapEitherT displayException id >>= getUrl)
+  (runEitherT $ getFile configFile & bimapEitherT displayException id >>= getUrl) >>= print
